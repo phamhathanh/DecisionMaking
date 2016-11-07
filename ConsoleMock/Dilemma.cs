84c274235065ec0d14b01ee5df71ba5bfd3ecd36
ThanhPH <phamhathanh130394@gmail.com>
2016-11-07 17:38:33 +0700

    Not sure what to write.

--- ConsoleMock/Dilemma.cs
+++ ConsoleMock/Dilemma.cs
@@ -1,33 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using static System.Math;
 
 namespace ConsoleMock
 {
     public class Dilemma
     {
-        private List<Choice> choices;
-        private List<Criterion> criteria;
+        private readonly List<string> choices;
+        private readonly List<Criterion> criteria;
 
-        public Dilemma(IEnumerable<Criterion> criteria)
+        private Func<string, Criterion, float> evaluation;
+        // Hack.
+        public Func<float, float> preferenceThreshold, indifferenceThreshold;
+
+        public Dilemma(IEnumerable<Criterion> criteria, IEnumerable<string> choices)
         {
-
+            this.criteria = criteria.ToList();
+            this.choices = choices.ToList();
         }
 
-        public Choice CreateChoice()
+        public void AddEvaluation(Func<string, Criterion, float> evaluation)
+            // Does not add, but set instead.
         {
-            throw new NotImplementedException();
+            this.evaluation = evaluation;
         }
 
-        public float GetPreference(Choice choice1, Choice choice2)
+        public float GetPreference(string choice1, string choice2, Criterion criterion)
+            // Implemented by Concordance Index
         {
+            var evaluationOfChoice1 = evaluation(choice1, criterion);
+            var evaluationOfChoice2 = evaluation(choice2, criterion);
 
+            var numerator = preferenceThreshold(evaluationOfChoice1) - Min(evaluationOfChoice2 - evaluationOfChoice1, preferenceThreshold(evaluationOfChoice1));
+            var denominator = preferenceThreshold(evaluationOfChoice1) - Min(evaluationOfChoice2 - evaluationOfChoice1, indifferenceThreshold(evaluationOfChoice1));
+            return numerator / denominator;
         }
 
         public float GetDiscredit(Choice choice1, Choice choice2)
         {
-
+            throw new NotImplementedException();
         }
     }
 }
--- ConsoleMock/Program.cs
+++ ConsoleMock/Program.cs
@@ -9,13 +9,43 @@ namespace ConsoleMock
     {
         public static void Main(string[] args)
         {
-            var price = new Criterion(1);
-            var funness = new Criterion(1);
+            var speed = new Criterion(1);
+            var criteria = new[] { speed };
+            var choices = new[] { "VW Golf C", "Renault R9 GTL", "Citroen GSA X1", "Peugeot P305 GR",
+                    "Talbot HOR.GLS", "Audi 80 CL", "Renault R18 GTL", "Alfa SUD TI-NR" };
+            var carDecision = new Dilemma(criteria, choices);
 
-            var dilemma = new Dilemma(new[] { price, funness });
+            carDecision.preferenceThreshold = gs => 0.1f * gs;
+            carDecision.indifferenceThreshold = gs => 5;
 
-            var firstChoice = dilemma.CreateChoice();
-            var secondChoice = dilemma.CreateChoice();
+            var speedByChoice = new Dictionary<string, float>()
+            {
+                ["VW Golf C"] = 140,
+                ["Renault R9 GTL"] = 150,
+                ["Citroen GSA X1"] = 160,
+                ["Peugeot P305 GR"] = 153,
+                ["Talbot HOR.GLS"] = 164,
+                ["Audi 80 CL"] = 148,
+                ["Renault R18 GTL"] = 155,
+                ["Alfa SUD TI-NR"] = 170
+            };
+            carDecision.AddEvaluation((choice, criterion) => speedByChoice[choice]);
+
+            var allPairs = from car1 in choices
+                           from car2 in choices
+                           where car1.CompareTo(car2) == -1
+                           select new Tuple<string, string>(car1, car2);
+
+            for (int i = 0; i < choices.Length; i++)
+            {
+                for (int j = 0; j < choices.Length; j++)
+                {
+                    var preference = carDecision.GetPreference(choices[i], choices[j], speed);
+                    Console.Write($"{preference:F3}\t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
         }
     }
 }
