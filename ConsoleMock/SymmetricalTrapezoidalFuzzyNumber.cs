e8f4bb497b63d4afcfdc1aff9c427bd9b7bacd1a
ThanhPH <phamhathanh130394@gmail.com>
2016-12-09 16:54:36 +0700

    Stuffs.

---
+++ ConsoleMock/SymmetricalTrapezoidalFuzzyNumber.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace ConsoleMock
+{
+    public class SymmetricalTrapezoidalFuzzyNumber : TrapezoidalFuzzyNumber
+    {
+        private readonly float center, indifferenceThreshold, preferenceThreshold;
+
+        public SymmetricalTrapezoidalFuzzyNumber(float center, float indifferenceThreshold, float preferenceThreshold)
+            : base(center - preferenceThreshold/2, center - indifferenceThreshold/2, center + indifferenceThreshold/2, center + preferenceThreshold/2)
+        {
+            this.center = center;
+            this.indifferenceThreshold = indifferenceThreshold;
+            this.preferenceThreshold = preferenceThreshold;
+        }
+    }
+}
