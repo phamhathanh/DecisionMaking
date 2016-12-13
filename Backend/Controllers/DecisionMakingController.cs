301022ee90cc6eba4cf659cf1e53e63eeed8b06a
ThanhPH <phamhathanh130394@gmail.com>
2016-12-13 09:38:11 +0700

    Backend.

---
+++ Backend/Controllers/DecisionMakingController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    public class DecisionMakingController : Controller
+    {
+        [HttpGet]
+        [Route("api/algorithm")]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+}
